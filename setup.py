bf5496cae02109957750c49778519ba8bb49866d
MilosAtz <matz@anl.gov>
2021-11-29 16:52:40 -0600

    dependency fixes for compatability with Python 3.6+

--- setup.py
+++ setup.py
@@ -104,7 +104,6 @@ if __name__ == "__main__":
                                   'tests/*.py']},
           include_package_data=True,
           classifiers=['Intended Audience :: Science/Research',
-                       'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7+',
                        'License :: OSI Approved :: BSD 3-Clause License',
